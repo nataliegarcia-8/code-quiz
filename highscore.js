a5936f95f1bf66f941ffc9bff50a5dced8e80216
Natalie Garcia <gnatalie.booking@gmail.com>
2020-07-09 22:34:02 -0500

    adding clearbutton, high score page and high score js, added timer

---
+++ highscore.js
@@ -0,0 +1,42 @@
+var scores = $("#scores");
+var userScore = $("#user-score");
+var highDiv = $("#highscores");
+var clear = $("#clear");
+
+clear.click(function () {
+    localStorage.clear();
+    location.reload();
+
+})
+
+function displayScore() {
+    score = JSON.parse(localStorage.getItem("score"));
+    var scoreDiv = $("<h1>");
+    scoreDiv.text(score + " of 5");
+    scores.append(scoreDiv);
+    var highscores = JSON.parse(localStorage.getItem("highscores"));
+    console.log(highscores)
+    if(highscores) {
+        highscores.push(score);
+    
+        for (var i = 0; i < highscores.length; i++) {
+            var highscoreDiv = $("<div>");
+            highscoreDiv.text(highscores[i]);
+            highDiv.append(highscoreDiv);
+                
+        }
+        highscores = [];
+    }
+if (highscores === null) {
+    localStorage.setItem("highscores", JSON.stringify([]));
+
+}
+else {
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+}
+
+
+
+
+displayScore();
--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 // question variables 
 var questions = [
-    ["question?", ["answer1", "answer2"], "answer1"],
-    ["question?", ["answer1", "answer2", "answer3"], "answer2"],
-    ["question?", ["answer1", "answer2", "answer3", "answer4"], "answer3"],
-    ["question?", ["answer1", "answer2", "answer3", "answer4"], "answer4"],
-    // question("question?", ["answer1", "answer2", "answer3", "answer4"], "correctAnswer"),
-    // question("question?", ["answer1", "answer2", "answer3", "answer4"], "correctAnswer"),
-    // question("question?", ["answer1", "answer2", "answer3", "answer4"], "correctAnswer"),
-    // question("question?", ["answer1", "answer2", "answer3", "answer4"], "correctAnswer"),
+    ["What does HTML stand for?", ["Hypertext Marketing Language", "Hypertrainer Marking Language", "Hypertext Markup Language", "Hyper Text Markup Leveler"], "Hypertext Markup Language"],
+    ["What do you use to style an HTML page?", ["HTML", "JavaScript", "Java", "CSS"], "CSS"],
+    ["What does CSS stand for?", ["Cascading Stem Sheets", "Cascading Style Sheets", "Cramming Styling Sheets", "Cramming Stem Sheets"], "Cascading Style Sheets"],
+    ["Which choice is a scripting language used to create and control dynamic website content?", ["JavaScript", "CSS", "HTML", "Bootstrap"], "JavaScript"],
+    ["Which choice is a popular CSS Framework for developing responsive websites?", ["JavaScript", "CSS", "HTML", "Bootstrap"], "Bootstrap"],
 ]
+// variables 
 var index = 0;
-
 
 var startBtn = $("#startbtn")
 var startScreen = $("#startScreen")
 var gameScreen = $("#gameBox")
 var questionDiv = $("#question")
 var btnBox = $("#btns")
+var scores = [];
+var score = 0;
+
+// start button - start page 
 
 startBtn.click(function () {
     startScreen.hide();
     gameScreen.show();
 
     question(questions[index])
+    
 })
-
-// var q = (whatever passed to function, questions[index])
+    var count = 50;
+    var interval = setInterval(function(){
+        document.getElementById('count').innerHTML = count;
+        count--;
+        if (count === 0){
+            clearInterval(interval);
+            alert("You're out of time!");
+        }
+    }, 1000);
+    
+// Display Question and Answer 
 function question(q) {
     btnBox.empty()
 
@@ -45,20 +56,25 @@ function question(q) {
     }
 }
 
+//check answer 
 function checkAnswer() {
     var choice = $(this).text()
     var correctAnswer = questions[index][2]
-    console.log(choice, correctAnswer)
-
+    if( choice === correctAnswer) {
+        score++;
+    }
+    
     index++;
     if (index < questions.length) {
         question(questions[index])
     } else {
         // window.location.href = window.location.href.replace("index", "highscore");
-
+        localStorage.setItem("score", JSON.stringify(score));
         var redirect = $("#redirect")
         redirect[0].click()
-        
-        console.log("Game over")
+
     }
 }
+
+localStorage.setItem("correctAnswer", JSON.stringify(correctAnswer));
+
